25b086d436ea657987a4fc6baa3ac8668bb61376
r-ca <nem@nem.x0.to>
2023-01-06 22:09:06 +0900

    動くようになった

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from starlette.exceptions import HTTPException
 from fastapi import FastAPI
 import sqlite3
 
@@ -5,12 +6,25 @@ app = FastAPI()
 
 exp_dbname = 'exp_table.db'
 
-@app.get("/kanmusu/")
-async def read_item(current: int = 1, terget: int = 175):
+@app.get("/exp/kanmusu/")
+async def read_item(current: int = 1, target: int = 175):
+
     connection = sqlite3.connect(exp_dbname)
     cursor = connection.cursor()
-    sql_current = 'SELECT exp FROM kanmusu WHERE 30 = level'
-    cursor.execute(sql_current);
-    return {"cur_lv_exp_test": cursor.fetchall()}
+
+    sql_current = 'SELECT exp FROM kanmusu WHERE ' + str(current) + ' = level'
+    cursor.execute(sql_current)
+    current_lv_exp = cursor.fetchone()
+    # current_lv_exp = current_lv_exp_list[0]
+
+    sql_target = 'SELECT exp FROM kanmusu WHERE ' + str(target) + ' = level'
+    cursor.execute(sql_target)
+    target_lv_exp = cursor.fetchone()
+    # target_lv_exp = target_lv_exp_list[0]
+
+    require_exp = target_lv_exp[0] - current_lv_exp[0]
+
+    return {"require_exp": str(require_exp)}
+
 
     
